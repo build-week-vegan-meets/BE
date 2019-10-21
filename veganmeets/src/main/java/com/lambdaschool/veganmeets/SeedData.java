fd97d30fb1e8ddd8da56fe34c59df444ad032b97
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-10-21 16:05:54 -0700

    updated to put back user Roles

--- veganmeets/src/main/java/com/lambdaschool/veganmeets/SeedData.java
+++ veganmeets/src/main/java/com/lambdaschool/veganmeets/SeedData.java
@@ -1,7 +1,10 @@
 package com.lambdaschool.veganmeets;
 
+import com.lambdaschool.veganmeets.models.Role;
 import com.lambdaschool.veganmeets.models.User;
+import com.lambdaschool.veganmeets.models.UserRoles;
 import com.lambdaschool.veganmeets.models.Useremail;
+import com.lambdaschool.veganmeets.services.RoleService;
 import com.lambdaschool.veganmeets.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,40 +18,65 @@ import java.util.ArrayList;
 public class SeedData implements CommandLineRunner
 {
     @Autowired
+    RoleService roleService;
+
+    @Autowired
     UserService userService;
 
 
     @Override
     public void run(String[] args) throws Exception
     {
-        User u1 = new User("admin", "password");
+        Role r1 = new Role("admin");
+        Role r2 = new Role("user");
+        Role r3 = new Role("data");
+
+        roleService.save(r1);
+        roleService.save(r2);
+        roleService.save(r3);
+
+        // admin, data, user
+        ArrayList<UserRoles> admins = new ArrayList<>();
+        admins.add(new UserRoles(new User(), r1));
+        admins.add(new UserRoles(new User(), r2));
+        admins.add(new UserRoles(new User(), r3));
+        User u1 = new User("admin", "password", admins);
         u1.getUseremails()
-          .add(new Useremail(u1, "admin@email.local"));
+                .add(new Useremail(u1, "admin@email.local"));
         u1.getUseremails()
-          .add(new Useremail(u1, "admin@mymail.local"));
+                .add(new Useremail(u1, "admin@mymail.local"));
 
         userService.save(u1);
 
         // data, user
-        User u2 = new User("cinnamon", "1234567");
+        ArrayList<UserRoles> datas = new ArrayList<>();
+        datas.add(new UserRoles(new User(), r3));
+        datas.add(new UserRoles(new User(), r2));
+        User u2 = new User("cinnamon", "1234567", datas);
         u2.getUseremails()
-          .add(new Useremail(u2, "cinnamon@mymail.local"));
+                .add(new Useremail(u2, "cinnamon@mymail.local"));
         u2.getUseremails()
-          .add(new Useremail(u2, "hops@mymail.local"));
+                .add(new Useremail(u2, "hops@mymail.local"));
         u2.getUseremails()
-          .add(new Useremail(u2, "bunny@email.local"));
+                .add(new Useremail(u2, "bunny@email.local"));
         userService.save(u2);
 
         // user
-        User u3 = new User("barnbarn", "ILuvM4th!");
+        ArrayList<UserRoles> users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u3 = new User("barnbarn", "ILuvM4th!", users);
         u3.getUseremails()
-          .add(new Useremail(u3, "barnbarn@email.local"));
+                .add(new Useremail(u3, "barnbarn@email.local"));
         userService.save(u3);
 
-        User u4 = new User("Bob", "password");
+        users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u4 = new User("Bob", "password", users);
         userService.save(u4);
 
-        User u5 = new User("Jane", "password");
+        users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u5 = new User("Jane", "password", users);
         userService.save(u5);
     }
 }
